4742113116f846312cd0c13aa6353e169202853b
sungyoungPark <sygitter1491012@gmail.com>
2021-01-23 03:40:23 +0900

    초기화면 구현

--- RxStudyMemo/View/ViewController/MemoListViewController.swift
+++ RxStudyMemo/View/ViewController/MemoListViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
    
     var viewModel: MemoListViewModel!
 
+   
+    @IBOutlet weak var listTableView: UITableView!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,17 +26,17 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
     
 
     func bindViewModel() {
+        viewModel.title   //뷰모델에 타이틀이 네비게이션에 추가
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "cell")) {
+                row, memo, cell in
+                cell.textLabel?.text = memo.content
+            }
+            .disposed(by: rx.disposeBag)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
